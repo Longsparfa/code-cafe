39240ee55956b0059f55f0d707a4a3370371fd56
Parfa Longs <longsparfa@gmail.com>
2024-07-01 14:51:14 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer, useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+import Header from './components/Header';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import Details from './components/Details';
+import DetailsItem from './components/DetailsItem';
+import { cartReducer, CartTypes, initialCartState } from './reducers/cartReducers';
+import CartPage from './components/CartPage';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [cart, dispatch] = useReducer(cartReducer, initialCartState);
+
+  const addToCart = (itemId) => {
+    dispatch({
+      type: CartTypes.ADD,
+      itemId,
+    });
+    console.log(itemId);
+  };
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const { data } = await axios.get('/api/items');
+        setItems(data);
+      } catch (error) {
+        console.error('Not reachable try again piease', error);
+      }
+    }
+    loadData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header cart={cart} />
+      {items.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home items={items} />} />
+          <Route path="/cart" element={<CartPage cart={cart} />} />
+          <Route path="/details" element={<Details items={items} />}>
+            <Route path=":id" element={<DetailsItem items={items} addToCart={addToCart} />} />
+            <Route index element={<div>No Item Selected</div>} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      )}
+    </Router>
   );
 }
 
